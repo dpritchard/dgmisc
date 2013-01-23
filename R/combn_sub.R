e668e2ce6cb3f4801b7e72ce02167ae1638acb9e
Daniel Pritchard <daniel@pritchard.co>
2013-01-23 21:19:01 +1000

    major changes to combn_sub()

--- R/combn_sub.R
+++ R/combn_sub.R
@@ -1,41 +1,60 @@
-combn_sub <- function(x, m, sub=NA){
-	if(is.na(sub)){
-		sub_undeclared = TRUE
-		sub <- choose(x,m)
-		if(sub==0){
-			stop('Number of possible combinations is zero.')
-		}
-	} else {
-		sub_undeclared = FALSE
+combn_sub <- function(n, m, sub=NA){
+    if(length(n)>1) {
+        stop("'n' must be a single integer. See help(combn_sub) for more info.")
+    }
+    
+	no_possible <- choose(n,m)
+	
+	if(no_possible==0){
+		stop('Number of possible combinations is zero.')
 	}
-	# Check if sub > number of unique combinations...
-	if(sub > choose(x,m)) {
+	
+	# If sub is undeclared, set it to the number of posisble combinations
+	sub_undeclared = FALSE
+	if(is.na(sub)){
+		sub <- no_possible
+		sub_undeclared = TRUE
+	}
+	
+	# If sub supplied, check if sub > number of unique combinations...
+	if(sub > choose(n,m)) {
 		stop('sub is greater than the number of unique combinations. sub must be <= choose(x,m).')
 	}
-	# OK, so the input should be OK at this point...
-	all_possible <- try(combn(x,m), silent=TRUE)
-	if(inherits(all_possible, 'try-error')) {
-		if(grep('too large or NA', all_possible[1])) {
-			if(sub_undeclared) {
-				stop("Too many items to iterate all possible combinations. You must supply 'sub'.")
-			}
-			warning('Too many items to iterate all possible combinations. Manually selecting a subset.')
-			outmat <- matrix(nrow=m, ncol=sub)
-			wsub = 1 
-			while(wsub <= sub) {
-				samp <- sample(c(1:x), m)
-				if(!any(colSums(outmat==samp, na.rm=TRUE)==nrow(outmat))) {
-					# The new column is unique. Add it.
-					outmat[,wsub] <- samp
-					wsub = wsub+1
-				}
-			}
+	
+	# For performace reasons, we limit the number of subsets to 5000
+	# NB: An arbirary limit.  Unlikely in ecology to need more than this?
+	if(sub > 5000) {
+		if(sub_undeclared) {
+			stop("You did not provide 'sub' and when trying to return all possible outcomes the (arbitary) 5000-item limit was exceeded. See help(combn_sub) for details.")
 		} else {
-			stop(cat('Unhandeled exception:\n', all_possible[1]))
+			stop('You requested more than 5000 unique combinations.  For performance reasons, the maximum number of posisble combinations is limited to 5000. See help(combn_sub) for details.')
+		}
+	}
+	
+	# If there are more than 500 thousand possible combinations then we will use the the manual (old school) approach.
+	# This also means that there will always be 1000-times more possible combinations available than the maximum requested.
+	# NB: This is also an arbitary limit base don performance on DP's MacBook.  
+	old_school <- FALSE
+	if(no_possible > 500000) {
+		old_school <- TRUE
+	}
+	
+	# OK, so the input should be OK at this point...
+	if(old_school) {
+		outmat <- matrix(nrow=m, ncol=sub)
+		wsub = 1 
+		while(wsub <= sub) {
+			samp <- sort(sample(c(1:n), m))
+			if(!any(colSums(outmat==samp, na.rm=TRUE)==nrow(outmat))) {
+				# The new column is unique. Add it.
+				outmat[,wsub] <- samp
+				wsub = wsub+1
+			}
 		}
 	} else {
+		all_possible <- combn(n,m)
 		all_subset <- sort(sample(c(1:dim(all_possible)[2]), sub))
 		outmat <- all_possible[,all_subset]
-	}
+	}	
 	return(outmat)
 }
