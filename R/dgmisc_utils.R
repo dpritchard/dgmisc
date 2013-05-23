6c24e6482db4322c6cdadffba27048d884b4b66e
Daniel Pritchard <daniel@pritchard.co>
2013-05-23 09:40:22 +0100

    Update message

--- R/dgmisc_utils.R
+++ R/dgmisc_utils.R
@@ -2,5 +2,5 @@
 
 ## The startup method
 .onAttach <- function(lib, pkg)  {
-    packageStartupMessage("This is dgmisc (v. ", utils::packageDescription("frair", field="Version"), appendLF = TRUE)
+    packageStartupMessage('This is dgmisc (v. ', utils::packageDescription("dgmisc", field="Version"), ')', appendLF = TRUE)
 }
